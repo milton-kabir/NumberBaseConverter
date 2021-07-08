f14419e791a14e41cebc35cd1a3ded7dbe3a1ff1
Milton Kabir <drifter.kabir@gmail.com>
2021-07-08 23:23:52 +0600

    Second Stage Completed

--- src/com/kabir/milton/Main.java
+++ src/com/kabir/milton/Main.java
@@ -6,21 +6,41 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Enter number in decimal system:");
         Scanner sc=new Scanner(System.in);
-        int x=sc.nextInt();
-        System.out.println("Enter target base:");
-        int b=sc.nextInt();
-        String res="";
-        if (b==2){
-            res=Integer.toBinaryString(x);
+        while(true){
+            System.out.println("Do you want to convert /from decimal or /to decimal? (To quit type /exit)");
+            String st=sc.nextLine();
+            if(st.equals("/from")){
+                System.out.println("Enter a number in decimal system:");
+                String s1=sc.nextLine();
+                int x=Integer.parseInt(s1);
+                System.out.println("Enter the target base:");
+                String s2=sc.nextLine();
+                int b=Integer.parseInt(s2);
+                String res;
+                if (b==2){
+                    res=Integer.toBinaryString(x);
+                }
+                else if(b==8){
+                    res=Integer.toOctalString(x);
+                }
+                else{
+                    res=Integer.toHexString(x);
+                }
+                System.out.println("Conversion to decimal result: "+res);
+            }
+            else if(st.equals("/to")){
+                System.out.println("Enter source number:");
+                String s1=sc.nextLine();
+                System.out.println("Enter source base:");
+                String s2=sc.nextLine();
+                int b=Integer.parseInt(s2);
+                System.out.println("Conversion to decimal result: "+Integer.parseInt(s1,b));
+            }
+            else{
+                break;
+            }
         }
-        else if(b==8){
-            res=Integer.toOctalString(x);
-        }
-        else{
-            res=Integer.toHexString(x);
-        }
-        System.out.println("Conversion result: "+res);
+
     }
 }
