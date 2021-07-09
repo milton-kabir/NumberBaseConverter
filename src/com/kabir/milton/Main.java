f11774edb399c18b8c9112d10f4660eb966bf8de
Milton Kabir <drifter.kabir@gmail.com>
2021-07-09 10:37:07 +0600

    Finally Commited

--- src/com/kabir/milton/Main.java
+++ src/com/kabir/milton/Main.java
@@ -1,9 +1,12 @@
 package com.kabir.milton;
 
+import java.math.BigDecimal;
 import java.math.BigInteger;
+import java.math.RoundingMode;
 import java.util.Scanner;
 
 public class Main {
+    public static String DIGITS = "0123456789abcdefghijklmnopqrstuvwxyz";
 
     public static void main(String[] args) {
 	// write your code here
@@ -20,7 +23,7 @@ public class Main {
                     if(ss.equals("/back")){
                         break;
                     }
-                    String ans=convertInteger(ss,a,b);
+                    String ans=fromDecimal(toDecimal(ss, a), b, 5);
                     System.out.println("Conversion result: "+ans);
                     System.out.println();
                 }
@@ -40,12 +43,51 @@ public class Main {
 
         }
     }
-    public static String convertInteger(String theValue, int initialBase, int finalBase) {
+    static String toDecimal(String number, int radix) {
+        BigDecimal result = BigDecimal.ZERO;
 
-        BigInteger bigInteger = new BigInteger(theValue,initialBase);
-        String value = bigInteger.toString(finalBase);
-        value = value.toUpperCase();
+        String[] array = number.split("\\.");
 
-        return value;
+        for (int i = 0; i < array[0].length(); i++) {
+            int val = DIGITS.indexOf(array[0].charAt(i));
+            result = result.add(BigDecimal.valueOf(val * Math.pow(radix, array[0].length() - 1 - i)));
+        }
+
+        if (array.length == 2) {
+            for (int i = 0; i < array[1].length(); i++) {
+                int val = DIGITS.indexOf(array[1].charAt(i));
+                result = result.add(BigDecimal.valueOf(val * Math.pow(radix, (i + 1) * -1)));
+            }
+        } else {
+            result = result.setScale(0);
+        }
+
+        return result.toString();
     }
+
+    static String fromDecimal(String number, int radix, int scale) {
+        StringBuilder result = new StringBuilder();
+
+        String[] array = number.split("\\.");
+        BigInteger integer = new BigDecimal(array[0]).toBigInteger();
+
+        do {
+            result.append(DIGITS.charAt(integer.divideAndRemainder(BigInteger.valueOf(radix))[1].intValue()));
+            integer = integer.divide(BigInteger.valueOf(radix));
+        } while (integer.compareTo(BigInteger.ZERO) > 0);
+        result = result.reverse();
+
+        if (array.length == 2) {
+            result.append(".");
+            BigDecimal remainder = new BigDecimal("0." + array[1]);
+            for (int i = 0; i < scale; i++) {
+                BigDecimal val = remainder.multiply(BigDecimal.valueOf(radix)).setScale(scale, RoundingMode.UP);
+                remainder = val.remainder(BigDecimal.ONE);
+                result.append(DIGITS.charAt(val.setScale(0, RoundingMode.DOWN).intValue()));
+            }
+        }
+
+        return result.toString();
+    }
+
 }
